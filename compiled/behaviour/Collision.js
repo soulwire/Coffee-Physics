ba14a0098cc4127f93bb414ec069840c461880b8
Justin Windle <j@qwiki.com>
2012-08-27 10:25:49 -0400

    Implement array slicing in collision loop to avoid out of bounds error. #5

--- compiled/behaviour/Collision.js
+++ compiled/behaviour/Collision.js
@@ -19,27 +19,27 @@ Collision = (function(_super) {
   }
 
   Collision.prototype.apply = function(p, dt, index) {
-    var dist, distSq, i, mt, o, overlap, r1, r2, radii, _i, _ref, _results;
+    var dist, distSq, mt, o, overlap, r1, r2, radii, _i, _len, _ref, _results;
+    _ref = this.pool.slice(index);
     _results = [];
-    for (i = _i = index, _ref = this.pool.length - 1; index <= _ref ? _i <= _ref : _i >= _ref; i = index <= _ref ? ++_i : --_i) {
-      o = this.pool[i];
-      if (o !== p) {
-        (this._delta.copy(o.pos)).sub(p.pos);
-        distSq = this._delta.magSq();
-        radii = p.radius + o.radius;
-        if (distSq <= radii * radii) {
-          dist = Math.sqrt(distSq);
-          overlap = (p.radius + o.radius) - dist;
-          overlap += 0.5;
-          mt = p.mass + o.mass;
-          r1 = this.useMass ? o.mass / mt : 0.5;
-          r2 = this.useMass ? p.mass / mt : 0.5;
-          p.pos.add(this._delta.clone().norm().scale(overlap * -r1));
-          o.pos.add(this._delta.norm().scale(overlap * r2));
-          _results.push(typeof this.callback === "function" ? this.callback(p, o, overlap) : void 0);
-        } else {
-          _results.push(void 0);
-        }
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      o = _ref[_i];
+      if (!(o !== p)) {
+        continue;
+      }
+      (this._delta.copy(o.pos)).sub(p.pos);
+      distSq = this._delta.magSq();
+      radii = p.radius + o.radius;
+      if (distSq <= radii * radii) {
+        dist = Math.sqrt(distSq);
+        overlap = (p.radius + o.radius) - dist;
+        overlap += 0.5;
+        mt = p.mass + o.mass;
+        r1 = this.useMass ? o.mass / mt : 0.5;
+        r2 = this.useMass ? p.mass / mt : 0.5;
+        p.pos.add(this._delta.clone().norm().scale(overlap * -r1));
+        o.pos.add(this._delta.norm().scale(overlap * r2));
+        _results.push(typeof this.callback === "function" ? this.callback(p, o, overlap) : void 0);
       } else {
         _results.push(void 0);
       }
